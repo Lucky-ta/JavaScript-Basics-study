f410a80cf1808a3ffb0ddcc5cde7ac2646071a46
Lukita <lucmaieski@gmail.com>
2022-10-26 14:38:27 -0300

    Cria funçoes de exemplos

---
+++ ExemplosJavaScript/FunctionsJs.js
@@ -0,0 +1,46 @@
+// Função de soma
+function somaDoisNumeros(num1, num2) {
+    return num1 + num2;
+}
+
+let resultado = somaDoisNumeros(5, 5);
+console.log(resultado);
+
+// Função de subtração
+
+function subtraiDoisNumeros(num1, num2) {
+    return num1 - num2;
+}
+
+let resultado2 = subtraiDoisNumeros(10, 5);
+console.log(resultado2)
+
+// Função de multiplicação
+
+function multiplicaDoisNumeros(num1, num2) {
+    return num1 * num2;
+}
+
+console.log(multiplicaDoisNumeros(5, 10));
+
+// Função de divisão
+
+function divideDoisNumeros(num1, num2) {
+    return num1 / num2;
+}
+
+console.log(divideDoisNumeros(8, 2));
+
+
+
+function comparaDoisNumeros(num1, num2) {
+    return num1 === num2;
+}
+
+console.log(comparaDoisNumeros(2, 1));
+
+
+function diferenciaDoisNumeros(p1, p2) {
+    return p1 !== p2;
+}
+console.log(diferenciaDoisNumeros(0, 1));
