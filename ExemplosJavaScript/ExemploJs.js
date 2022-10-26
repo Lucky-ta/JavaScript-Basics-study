c923899f0d6b6842f04ed4e0da440045638bb60f
Lukita <lucmaieski@gmail.com>
2022-10-26 14:05:45 -0300

    Adiciona exemplos

---
+++ ExemplosJavaScript/ExemploJs.js
@@ -0,0 +1,32 @@
+let num1 = 5;
+let num2 = 10;
+let num3 = 15;
+
+//soma
+let sum = num1 + num2;
+console.log(sum);
+
+// Subtração
+let subtraction = num1 - num2;
+console.log(subtraction);
+
+// Divisão
+let division = num1 / num2;
+console.log(division);
+
+// Multiplicação
+let multiplication = num1 * num2;
+console.log(multiplication);
+
+
+
+// Operador de relação
+let comparation = num1 == num2;
+console.log(comparation);
+
+
+// Como saber se o número é par?
+let number = 1284791098433;
+
+let isEven = (number % 2) === 0;
+console.log(isEven);
