9d90e947b35111eefd01f3df5de7f0939e0532d0
Lukita <lucmaieski@gmail.com>
2022-10-26 15:05:17 -0300

    Adiciona comentarios de console logs

--- ExemplosJavaScript/Calculadora.js
+++ ExemplosJavaScript/Calculadora.js
@@ -20,5 +20,15 @@ function calculadora(num1, num2, operacao) {
     }
 }
 
+// soma
+console.log(calculadora(2, 3, '+'));
 
+// subtração
+console.log(calculadora(2, 3, '-'));
+
+// divisão
+console.log(calculadora(2, 3, '/'));
+
+// multiplicação
 console.log(calculadora(2, 3, '*'));
+
